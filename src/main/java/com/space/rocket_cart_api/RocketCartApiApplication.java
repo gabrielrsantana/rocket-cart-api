7af7afc13baac0bc17685879d5361e14793cd705
Gabriel <gabriel.rsantana@hotmail.com>
2025-06-10 23:37:29 -0300

    initial criation of project TODO: configure a DataSource

---
+++ src/main/java/com/space/rocket_cart_api/RocketCartApiApplication.java
@@ -0,0 +1,13 @@
+package com.space.rocket_cart_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketCartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketCartApiApplication.class, args);
+	}
+
+}
